80ba44ce66bd11d278e0bd6dc91f01e3e1078ab4
Naboulibear <108967590+Naboulibear@users.noreply.github.com>
2026-05-27 00:19:08 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -13,15 +13,36 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# Create the jackson family object
-jackson_family = FamilyStructure("Jackson")
-
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+# Create the jackson family object
+jackson_family = FamilyStructure("Jackson")
+
+initial_data = [{
+    "id": 3443,
+    "first_name":"Tommy" ,
+    "last_name":"Jackson",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}, {
+    "id": 3444,
+    "first_name":"Jane" ,
+    "last_name":"Jackson",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}, {
+    "id": 3445,
+    "first_name":"Jimmy" ,
+    "last_name":"Jackson",
+    "age": 5,
+    "lucky_numbers": [1]
+}]
+
+for members_data in initial_data:
+    jackson_family.add_member(members_data)
 
 # Generate sitemap with all your endpoints
 @app.route('/')
@@ -33,11 +54,25 @@ def sitemap():
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
 
+@app.route('/member', methods=['POST'])
+def add_new_member():
+    member_request_body = request._json(force = True)
+    member_request_body = jackson_family.add_member(member_request_body)
+    return jsonify(member_request_body), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delte_single_member(id):
+    if jackson_family.delete_member(id):
+        return jsonify({'done':True}),200
+    else: 
+        return jsonify({'done':False}),404
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -7,38 +7,34 @@ Update this file to implement the following already declared methods:
 
 class FamilyStructure:
     def __init__(self, last_name):
-        self.last_name = last_name
-        self._next_id = 1
-        self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            }
-        ]
+       self.last_name = last_name
+       self._members = []
 
     # This method generates a unique incremental ID
-    def _generate_id(self):
-        generated_id = self._next_id
-        self._next_id += 1
-        return generated_id
+    def _generateId(self):
+        return randint(0,9999)
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
+        if "id" not in member:
+            member.update(
+                id = self._generateId
+            )
+        self._members.append(member)
         pass
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for member in self._members:
+            if member ["id"] == id:
+                self._members.remove(member)
+                return "true"
+        return False
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        for member in self._members:
+            if member ["id"] == id:
+                return member
+        return None      
+        
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
